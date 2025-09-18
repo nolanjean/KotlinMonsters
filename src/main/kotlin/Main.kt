1d14344192a93bb51809cd25aa059de053e15a4a
nolanjean <nobodyplayeur@gmail.com>
2025-09-18 10:20:17 +0200

    ajout class EspeceMonstre + test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,16 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    println(changeCouleur("Hello","rouge"))
-    println(changeCouleur("World","bleu"))
-    println("Hello ${changeCouleur("my","jaune")} World")
-    println(changeCouleur("Truc","marron"))
-}
+import org.example.dresseur.Entraineur
 
+/**
+ * Change la couleur du message donné selon le nom de la couleur spécifié.
+ * Cette fonction utilise les codes d'échappement ANSI pour appliquer une couleur à la sortie console. Si un nom de couleur
+ * non reconnu ou une chaîne vide est fourni, aucune couleur n'est appliquée.
+ *
+ * @param message Le message auquel la couleur sera appliquée.
+ * @param couleur Le nom de la couleur à appliquer (ex: "rouge", "vert", "bleu"). Par défaut c'est une chaîne vide, ce qui n'applique aucune couleur.
+ * @return Le message coloré sous forme de chaîne, ou le même message si aucune couleur n'est appliquée.
+ */
 
 fun changeCouleur(message: String, couleur:String=""): String {
     val reset = "\u001B[0m"
@@ -20,7 +22,21 @@ fun changeCouleur(message: String, couleur:String=""): String {
         "magenta" -> "\u001B[35m"
         "cyan" -> "\u001B[36m"
         "blanc" -> "\u001B[37m"
+        "marron" -> "\u001B[38;5;94m"
         else -> "" // pas de couleur si non reconnu
     }
     return "$codeCouleur$message$reset"
+}
+
+
+var joueur = Entraineur(1,"Sacha",100)
+var rival = Entraineur(2,"Regis",200)
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    joueur.afficheDetail()
+    rival.afficheDetail()
+    joueur.argents+=50
+    joueur.afficheDetail()
 }
