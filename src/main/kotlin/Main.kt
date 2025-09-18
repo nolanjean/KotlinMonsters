6816d1df08d369f1040a6aec7ee7606074bd48c5
nolanjean <nobodyplayeur@gmail.com>
2025-09-18 09:04:58 +0200

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,26 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    println(changeCouleur("Hello","rouge"))
+    println(changeCouleur("World","bleu"))
+    println("Hello ${changeCouleur("my","jaune")} World")
+    println(changeCouleur("Truc","marron"))
+}
+
+
+fun changeCouleur(message: String, couleur:String=""): String {
+    val reset = "\u001B[0m"
+    val codeCouleur = when (couleur.lowercase()) {
+        "rouge" -> "\u001B[31m"
+        "vert" -> "\u001B[32m"
+        "jaune" -> "\u001B[33m"
+        "bleu" -> "\u001B[34m"
+        "magenta" -> "\u001B[35m"
+        "cyan" -> "\u001B[36m"
+        "blanc" -> "\u001B[37m"
+        else -> "" // pas de couleur si non reconnu
+    }
+    return "$codeCouleur$message$reset"
+}
